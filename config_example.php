b0347647b0e645a87bf39511a7b5cb1b66f38781
Juan Gabriel Maioli <juanmaioli@gmail.com>
2024-01-09 12:54:48 -0300

    Add images

--- config_example.php
+++ config_example.php
@@ -1,12 +1,13 @@
 <?php
+	//Edit and change name to config.php
 	$db_server = "localhost";
 	$db_user = "root";
 	$db_pass = "";
-	$db_name = "template_login";
+	$db_name = "drawers";
 	$db_serverport = "3306";
-	$table_pre = "template_";
-	$www_host = "192.168.0.12";
+	$table_pre = "drawers_";
+	$www_host = "localhost";
 	$www_https = "";
-	$site_cookie = "templateID";
+	$site_cookie = "drawersID";
 	$contact_mail="contact@example.com";
 ?>
